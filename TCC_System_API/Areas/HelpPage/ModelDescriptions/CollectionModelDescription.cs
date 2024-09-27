4bd43d05b1d08b788e05d93c930c32392e02d68d
Kevin Romeiro Barbosa <qo1barboke@group.pirelli.com>
2024-09-27 10:14:05 -0300

    removido

--- TCC_System_API/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace TCC_System_API.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
