a4b14086e88200a14fe23a5c3fdb0f70fa8ac309
Kevin Romeiro Barbosa <qo1barboke@group.pirelli.com>
2024-10-24 00:38:02 -0300

    começo

---
+++ TCC_System_Domain/Blog/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TCC_System_Domain.Blog
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ TCC_System_Domain/Blog/Post.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TCC_System_Domain.Core;
+using TCC_System_Domain.Management;
+
+namespace TCC_System_Domain.Blog
+{
+    public class Post : Entity, IAggregateRoot
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public User User { get; set; }
+
+        private readonly List<CategoryPost> _categoryPosts;
+        public IReadOnlyCollection<CategoryPost> CategoryPosts => _categoryPosts;
+
+        public Post(Guid id, string title, string body, User user)
+        {
+            _categoryPosts = new List<CategoryPost>();
+            SetID(id);
+            SetTitle(title);
+            SetBody(body);
+            SetUser(user);
+        }
+
+        public void SetID(Guid id) => this.Id = id;
+        public void SetTitle(string title) => this.Title = title;
+        public void SetBody(string body) => this.Body = body;
+        public void SetUser(User user) => this.User = user;
+
+
+    }
+}
